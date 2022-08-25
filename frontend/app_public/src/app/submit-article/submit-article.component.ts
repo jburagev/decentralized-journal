d4fc163687eb13771d43b24c0c5fabc267af8c74
jburagev <buragev@gmail.com>
2022-08-25 20:50:03 +0200

    add subm article authorize user

--- frontend/app_public/src/app/submit-article/submit-article.component.ts
+++ frontend/app_public/src/app/submit-article/submit-article.component.ts
@@ -33,6 +33,65 @@ export class SubmitArticleComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
+  account:any = ""
+  foundAccountMetamask:boolean = false
+  foundAccountJournal:boolean = false
+  userAuthorized:boolean = false
+  userType:string = ""
+
+  vrniUporabnika = async (): Promise<string> => {
+    const ethereum = window.ethereum as MetaMaskInpageProvider;
+
+
+    if (typeof window.ethereum !== "undefined") {
+      // Poveži se na MetaMask
+      const racuni: any = await ethereum.request({method: "eth_requestAccounts"});
+
+      
+      
+      if (racuni != null) {
+        //alert(racuni[0]);
+        this.foundAccountMetamask = true;
+        console.log(racuni[0])
+
+          this.http.get<any>('http://localhost:8083/authorize/' + racuni[0]).subscribe({
+            next: data => {
+                console.log(data);
+                if(data != 0 && data != 1 && data != 2 && data != 3){
+                  this.foundAccountJournal = false;
+                  
+                }else{
+                  this.foundAccountJournal = true;
+                  if(data == 0){
+                    this.userType = "Reader";
+                  }
+                  if(data == 1){
+                    this.userType = "Reviewer";
+                    this.userAuthorized = true;
+                  }
+                  if(data == 2){
+                    this.userType = "Editor";
+                    this.userAuthorized = true;
+                  }
+                  if(data == 3){
+                    this.userType = "Author";
+                    this.userAuthorized = true;
+                  }
+                }
+
+            },
+            error: error => {
+              
+                console.error('There was an error!', error);
+            }
+          })
+
+        return racuni[0];
+      } else return ""
+    } else return "";
+  }
+
+
   submitArticle = async (): Promise<any> => {
 
     const ethereum = window.ethereum as MetaMaskInpageProvider;
@@ -114,6 +173,7 @@ export class SubmitArticleComponent implements OnInit {
   ngOnInit(): void {
 
     //this.getArticleByHash();
+    this.account = this.vrniUporabnika();
 
   }
 
